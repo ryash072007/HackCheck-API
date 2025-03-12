d814700aed619fe1b2ca756168784d21a93987f2
Yash Raj <ryash072007@gmail.com>
2025-03-12 11:52:06 +0400

    registered new models with admin

--- db/admin.py
+++ db/admin.py
@@ -4,3 +4,5 @@ from db.models import *
 # Register your models here.
 admin.site.register(Account)
 admin.site.register(TeamProfile)
+admin.site.register(Question)
+admin.site.register(Answer)
--- db/models/question.py
+++ db/models/question.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.db import models
-from db.models.user import TeamMember
+from .user import TeamMember
 
 class Question(models.Model):
     """
