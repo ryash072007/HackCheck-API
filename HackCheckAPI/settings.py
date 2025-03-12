def7ecbc877a214c142d637d8814efcf0b1aa312
Yash Raj <ryash072007@gmail.com>
2025-03-12 23:30:02 +0400

    updated cors to wrk with frontend

--- HackCheckAPI/settings.py
+++ HackCheckAPI/settings.py
@@ -53,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
 ]
 
 SIMPLE_JWT = {
