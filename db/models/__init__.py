703da00f886be401b3d85a081ff1e448d6b92535
Yash Raj <ryash072007@gmail.com>
2025-03-12 21:56:15 +0400

    added hackathon settings model

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -1,2 +1,3 @@
 from db.models.user import *
 from db.models.question import *
+from db.models.hackathon import *
---
+++ db/models/hackathon.py
@@ -0,0 +1,26 @@
+from datetime import timedelta
+from django.core.exceptions import ValidationError
+from django.db import models
+
+class HackathonSettings(models.Model):
+    max_team_size = models.PositiveIntegerField(default=4)
+    duration = models.DurationField(default=timedelta(hours=3))
+
+    has_started = models.BooleanField(default=False)
+    has_ended = models.BooleanField(default=False)
+    
+    time_started = models.DateTimeField(null=True, blank=True)
+    time_ended = models.DateTimeField(null=True, blank=True)
+    
+    class Meta:
+        verbose_name_plural = "Hackathon Settings"
+    
+    def save(self, *args, **kwargs):
+        if HackathonSettings.objects.exists() and not self.pk:
+            raise ValidationError("Only one instance of HackathonSettings can exist")
+        return super().save(*args, **kwargs)
+    
+    @classmethod
+    def get_instance(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
