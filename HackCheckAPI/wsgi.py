b5c1d08460046f50479d9b2d3d5da9c55aba9f42
Yash Raj <ryash072007@gmail.com>
2025-03-24 16:23:15 +0400

    Comment out database path logging and migration command in wsgi.py

--- HackCheckAPI/wsgi.py
+++ HackCheckAPI/wsgi.py
@@ -10,15 +10,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
+# from django.core.management import call_command
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HackCheckAPI.settings')
 
 application = get_wsgi_application()
 
-print(f"{os.path.join('/tmp', 'db.sqlite3')}, {os.path.exists(os.path.join('/tmp', 'db.sqlite3'))}")
-# Apply all migrations
-call_command("migrate")
+# print(f"{os.path.join('/tmp', 'db.sqlite3')}, {os.path.exists(os.path.join('/tmp', 'db.sqlite3'))}")
+# # Apply all migrations
+# call_command("migrate")
 
-print(f"{os.path.join('/tmp', 'db.sqlite3')}, {os.path.exists(os.path.join('/tmp', 'db.sqlite3'))}")
+# print(f"{os.path.join('/tmp', 'db.sqlite3')}, {os.path.exists(os.path.join('/tmp', 'db.sqlite3'))}")
